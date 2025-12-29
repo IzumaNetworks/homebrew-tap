b735c8fca42b62925f1b68761c389b26af5f3007
Travis McCollum <travis.mccollum@izumanetworks.com>
2025-12-29 15:08:56 -0700

    Add stressman formula (placeholder)

---
+++ Formula/stressman.rb
@@ -0,0 +1,78 @@
+class Stressman < Formula
+  desc "Distributed stress testing orchestration platform"
+  homepage "https://github.com/IzumaNetworks/stressman"
+  version "0.0.1"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/IzumaNetworks/stressman/releases/download/v0.0.1/stressman-darwin-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/IzumaNetworks/stressman/releases/download/v0.0.1/stressman-darwin-amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/IzumaNetworks/stressman/releases/download/v0.0.1/stressman-linux-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/IzumaNetworks/stressman/releases/download/v0.0.1/stressman-linux-amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    if OS.mac?
+      if Hardware::CPU.arm?
+        bin.install "stressman-darwin-arm64" => "stressman"
+        bin.install "stressagent-darwin-arm64" => "stressagent"
+      else
+        bin.install "stressman-darwin-amd64" => "stressman"
+        bin.install "stressagent-darwin-amd64" => "stressagent"
+      end
+    else
+      if Hardware::CPU.arm?
+        bin.install "stressman-linux-arm64" => "stressman"
+        bin.install "stressagent-linux-arm64" => "stressagent"
+      else
+        bin.install "stressman-linux-amd64" => "stressman"
+        bin.install "stressagent-linux-amd64" => "stressagent"
+      end
+    end
+
+    # Install config directory
+    (etc/"stressman").mkpath
+    (var/"stressman").mkpath
+    (var/"log/stressman").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      To start the stressman server:
+        stressman server
+
+      To connect with TUI from anywhere:
+        stressman tui --server=<host>:9842 --token=<token>
+
+      Configuration file location:
+        #{etc}/stressman/stressman.yaml
+    EOS
+  end
+
+  service do
+    run [opt_bin/"stressman", "server", "--config", etc/"stressman/stressman.yaml"]
+    keep_alive true
+    log_path var/"log/stressman/stressman.log"
+    error_log_path var/"log/stressman/stressman.log"
+    working_dir var/"stressman"
+  end
+
+  test do
+    assert_match "stressman", shell_output("#{bin}/stressman version")
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# homebrew-tap
-a tap for distributing home-brews
+# Izuma Networks Homebrew Tap
+
+Homebrew formulae for Izuma Networks software.
+
+## Installation
+
+```bash
+brew tap IzumaNetworks/tap
+```
+
+## Available Formulae
+
+### stressman
+
+Distributed stress testing orchestration platform.
+
+```bash
+# Install
+brew install IzumaNetworks/tap/stressman
+
+# Start server as a service
+brew services start stressman
+
+# Or run manually
+stressman server
+
+# Connect TUI from anywhere
+stressman tui --server=<host>:9842 --token=<token>
+```
+
+## License
+
+Apache-2.0
